8581ef2e23ef20964fa41a45403d28c052850328
rorshaksama <cmd.rorshak@laposte.net>
2020-03-02 09:23:53 +0100

    Merge origin/master
    Conflicts:
    	src/java/fr/solutec/dao/ClientDao.java

--- src/java/fr/solutec/servlet/MessagerieServlet.java
+++ src/java/fr/solutec/servlet/MessagerieServlet.java
@@ -58,7 +58,7 @@ public class MessagerieServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        request.getRequestDispatcher("WEB-INF/messagerieClient.jsp").forward(request, response);
+        request.getRequestDispatcher("WEB-INF/messagerieConseiller.jsp").forward(request, response);
     }
 
     /**
